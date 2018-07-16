0124399e8300fc3ebf453be56cc9231d26334c53
wendysoto <38251149+wendysoto@users.noreply.github.com>
2018-07-16 07:32:07 -0500

    Add files via upload

---
+++ herencia/src/herencia/Profesor.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author Wendy Soto
+ */
+public class Profesor extends Persona {
+    
+    private String facultad;
+    private String materia;
+    
+    public Profesor(String nombre, String apellido, int edad, String facultad, String materia) {
+        super(nombre, apellido, edad);
+        this.facultad=facultad;
+        this.materia=materia;
+        
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+    
+}
